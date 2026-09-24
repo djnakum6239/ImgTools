b48d0f0ee6b51bf6c4d98ad12f6a20f2fc945036
djnakum6239 <d.j.nakum6239@gmail.com>
2026-09-24 11:50:59 +0530

    Raise compile SDK for current Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.djnakum.imgtools"
-    compileSdk = 35
+    compileSdk = 37
     defaultConfig {
         applicationId = "com.djnakum.imgtools"
         minSdk = 26
